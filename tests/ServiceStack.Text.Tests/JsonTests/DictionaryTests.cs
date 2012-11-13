e952938d98468473ae9048cc5e003e2ebb3d8596
Dan Mannock <daniel.mannock@dayinsure.com>
2012-11-13 17:24:15 +0000

    Dictionary unit tests added, mainly for the issue when serializing a dictionary using a complex object as a key

--- tests/ServiceStack.Text.Tests/JsonTests/DictionaryTests.cs
+++ tests/ServiceStack.Text.Tests/JsonTests/DictionaryTests.cs
@@ -111,5 +111,86 @@ namespace ServiceStack.Text.Tests.JsonTests
             var resultJson = result.ToJson();
             Assert.AreEqual(json, resultJson);
         }
+
+        [Test]
+        public void CanSerializeAndDeserializeNonStringKey()
+        {
+            var original = new Dictionary<Guid, string>{
+                { Guid.NewGuid(), "test" },
+            };
+
+            var json = original.ToJson();
+            Console.WriteLine(json);
+
+            var result = JsonSerializer.DeserializeFromString<Dictionary<Guid, string>>(json);
+            var resultJson = result.ToJson();
+            Assert.AreEqual(json, resultJson);
+        }
+
+        [Test]
+        public void CanSerializeAndDeserializeObjectsWithStringKey()
+        {
+            var original = new ClassObjDictString
+            {
+                MyProperty = new Dictionary<string, Obj2>
+                {
+                    { "key1", new Obj2 { P1 = "1", P2 = "2" } },
+                }
+            };
+
+            var json = original.ToJson();
+            Console.WriteLine(json);
+
+            var result = JsonSerializer.DeserializeFromString<ClassObjDictString>(json);
+            var resultJson = result.ToJson();
+            Assert.AreEqual(json, resultJson);
+        }
+
+        [Test]
+        public void CanSerializeAndDeserializeObjectAsKey()
+        {
+            var original = new ClassObjDict
+            {
+                MyProperty = new Dictionary<Obj1,Obj2>
+                {
+                   { 
+                       new Obj1 { MyString = "MyStringValue", MyInt = 1 }, 
+                       new Obj2 { P1 = "1", P2 = "2" } 
+                    },
+                }
+            };
+
+            var json = original.ToJson();
+            Console.WriteLine(json);
+
+            var result = JsonSerializer.DeserializeFromString<ClassObjDict>(json);
+            var resultJson = result.ToJson();
+            Assert.AreEqual(json, resultJson);
+        }
+
+        class ClassObjDictString
+        {
+            public Dictionary<string, Obj2> MyProperty { get; set; }
+        }
+
+        [Serializable]
+        class ClassObjDict
+        {
+            public Dictionary<Obj1, Obj2> MyProperty { get; set; }
+        }
+
+        [Serializable]
+        class Obj1
+        {
+            public String MyString { get; set; }
+            public int MyInt { get; set; }
+        }
+
+        [Serializable]
+        class Obj2
+        {
+            public String P1 { get; set; }
+            public String P2 { get; set; }
+        }
 	}
 }
